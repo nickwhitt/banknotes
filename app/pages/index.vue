685c898d28cd23b6e9de484cf1f412159866a1a7
Nick Whitt <nick.whitt@protonmail.com>
2025-02-28 16:45:58 -0500

    table scaffold

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,51 +1,44 @@
 <template>
   <UContainer>
-    <h1 class="text-3xl font-semibold tracking-tight text-pretty sm:text-4xl">
+    <h1 class="my-8 text-3xl sm:text-4xl font-semibold tracking-tight text-pretty">
       $5 Federal Reserve Note
     </h1>
 
-    <p class="mt-6 text-lg/8">
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum officia iure maxime repudiandae delectus ea, quis
-      nam pariatur, at repellat accusamus? Harum deserunt doloribus quia architecto nulla suscipit. Repellat,
-      praesentium.
-    </p>
-
-    <h2 class="text-2xl font-semibold tracking-tight text-pretty sm:text-3xl">
-      Series {{ series.series }}
+    <h2 class="my-6 text-2xl sm:text-3xl font-semibold tracking-tight text-pretty">
+      Series 2021
     </h2>
 
-    <ul role="list" class="grid gap-4 md:grid-cols-2 xl:grid-cols-3 my-8">
-      <li v-for="run in series.runs">
-        <UCard>
+    <h3 class="my-4 text-xl sm:text-2xl font-semibold tracking-tight text-pretty">
+      Boston
+    </h3>
+
+    <ol class="grid gap-4 md:grid-cols-2 xl:grid-cols-3">
+      <li>
+        <UCard :ui="{ body: 'p-0 sm:p-0' }">
           <template #header>
-            <div class="flex items-center justify-between">
-              <h3 class="text-base/7 font-semibold tracking-tight uppercase">{{ run.bank.city }}</h3>
-              <div class="space-x-2">
-                <UBadge v-if="run.dc" label="DC" variant="outline" color="success" />
-                <UBadge v-if="run.fw" label="FW" variant="outline" color="error" />
-              </div>
+            <div class="flex justify-between">
+              <p class="font-semibold text-pretty">Regular Notes</p>
+              <p>{{ formatNumber(57600000 - 1 + 1) }}</p>
             </div>
           </template>
 
-          <div class="flex flex-wrap justify-evenly gap-x-6 gap-y-4 text-nowrap">
-            <ul>
-              <li class="text-center tracking-tight text-pretty">
-                {{ formatNumber(run.regular.count) }} Regular
-              </li>
-              <li class="font-mono">{{ formatSerialNumber(run.regular.low, run.bank, series.letter) }}</li>
-              <li class="font-mono">{{ formatSerialNumber(run.regular.high, run.bank, series.letter) }}</li>
-            </ul>
-            <ul v-if="run.star">
-              <li class="text-center tracking-tight text-pretty">
-                {{ formatNumber(run.star.count) }} Star
-              </li>
-              <li class="font-mono">{{ formatSerialNumber(run.star.low, run.bank, series.letter) }}</li>
-              <li class="font-mono">{{ formatSerialNumber(run.star.high, run.bank, series.letter) }}</li>
-            </ul>
-          </div>
+          <UTable :data="data" :ui="{ thead: '[&>tr]:after:h-0' }">
+            <template #serials-cell="{ row }">
+              <ul class="font-mono">
+                <li>{{ formatSerialNumber(row.original.serials.start, { letter: 'A', city: 'Boston' }, 'Q') }}</li>
+                <li>{{ formatSerialNumber(row.original.serials.end, { letter: 'A', city: 'Boston' }, 'Q') }}</li>
+              </ul>
+            </template>
+            <template #printed-cell="{ row }">
+              <ul class="text-right">
+                <li>{{ formatNumber(row.original.serials.end.serial - row.original.serials.start.serial + 1) }}</li>
+                <li>{{ row.original.printed }}</li>
+              </ul>
+            </template>
+          </UTable>
         </UCard>
       </li>
-    </ul>
+    </ol>
   </UContainer>
 </template>
 
@@ -58,27 +51,6 @@ type FederalReserveBank = {
 type SerialBlock = {
   serial: number
   block: string
-}
-
-type SerialRange = {
-  count: number
-  low: SerialBlock
-  high: SerialBlock
-}
-
-type PrintRun = {
-  bank: FederalReserveBank
-  regular: SerialRange
-  star?: SerialRange
-  dc?: boolean
-  fw?: boolean
-}
-
-type Series = {
-  series: string
-  denomination: number
-  letter: string
-  runs: PrintRun[]
 }
 
 function formatNumber(value: number): string {
@@ -100,77 +72,8 @@ function formatSerialNumber(serialBlock: SerialBlock, bank: FederalReserveBank, 
   return `${seriesLetter}${bank.letter} ${serialNumber.substring(0, 4)} ${serialNumber.substring(4)} ${serialBlock.block}`
 }
 
-const series: Series = {
-  series: '2021',
-  denomination: 5,
-  letter: 'Q',
-  runs: [
-    {
-      bank: { letter: 'A', city: 'Boston' },
-      regular: { count: 5760000, low: { serial: 1, block: 'A' }, high: { serial: 57600000, block: 'A' } },
-      star: { count: 480000, low: { serial: 1, block: '*' }, high: { serial: 480000, block: '*' } },
-      fw: true,
-    },
-    {
-      bank: { letter: 'B', city: 'New York' },
-      regular: { count: 192000000, low: { serial: 1, block: 'A' }, high: { serial: 96000000, block: 'B' } },
-      star: { count: 640000, low: { serial: 1, block: '*' }, high: { serial: 640000, block: '*' } },
-      fw: true,
-    },
-    {
-      bank: { letter: 'C', city: 'Philadelphia' },
-      regular: { count: 51200000, low: { serial: 1, block: 'A' }, high: { serial: 51200000, block: 'A' } },
-      fw: true,
-    },
-    {
-      bank: { letter: 'D', city: 'Cleveland' },
-      regular: { count: 179200000, low: { serial: 1, block: 'A' }, high: { serial: 83200000, block: 'B' } },
-      fw: true,
-    },
-    {
-      bank: { letter: 'E', city: 'Richmond' },
-      regular: { count: 262400000, low: { serial: 1, block: 'A' }, high: { serial: 70400000, block: 'C' } },
-      fw: true,
-    },
-    {
-      bank: { letter: 'F', city: 'Atlanta' },
-      regular: { count: 243200000, low: { serial: 1, block: 'A' }, high: { serial: 51200000, block: 'C' } },
-      star: { count: 640000, low: { serial: 1, block: '*' }, high: { serial: 640000, block: '*' } },
-      fw: true,
-    },
-    {
-      bank: { letter: 'G', city: 'Chicago' },
-      regular: { count: 224000000, low: { serial: 1, block: 'A' }, high: { serial: 32000000, block: 'B' } },
-      fw: true,
-    },
-    {
-      bank: { letter: 'H', city: 'St. Louis' },
-      regular: { count: 83200000, low: { serial: 1, block: 'A' }, high: { serial: 83200000, block: 'A' } },
-      star: { count: 640000, low: { serial: 1, block: '*' }, high: { serial: 640000, block: '*' } },
-      fw: true,
-    },
-    {
-      bank: { letter: 'I', city: 'Minneapolis' },
-      regular: { count: 32000000, low: { serial: 1, block: 'A' }, high: { serial: 32000000, block: 'A' } },
-      star: { count: 3200000, low: { serial: 1, block: '*' }, high: { serial: 3200000, block: '*' } },
-      fw: true,
-    },
-    {
-      bank: { letter: 'J', city: 'Kansas City' },
-      regular: { count: 25600000, low: { serial: 1, block: 'A' }, high: { serial: 25600000, block: 'A' } },
-      star: { count: 3200000, low: { serial: 1, block: '*' }, high: { serial: 3200000, block: '*' } },
-      fw: true,
-    },
-    {
-      bank: { letter: 'K', city: 'Dallas' },
-      regular: { count: 153600000, low: { serial: 1, block: 'A' }, high: { serial: 57600000, block: 'B' } },
-      fw: true,
-    },
-    {
-      bank: { letter: 'L', city: 'San Francisco' },
-      regular: { count: 249600000, low: { serial: 1, block: 'A' }, high: { serial: 57600000, block: 'C' } },
-      fw: true,
-    },
-  ],
-}
+const data = [
+  { date: 'Nov 2022', serials: { start: { serial: 1, block: 'A' }, end: { serial: 32000000, block: 'A' } }, printed: 'Fort Worth' },
+  { date: 'Dec 2022', serials: { start: { serial: 32000001, block: 'A' }, end: { serial: 57600000, block: 'A' } }, printed: 'Fort Worth' },
+]
 </script>
