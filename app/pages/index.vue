a757ebe8b7e4abad6b67eee826a020553636e22a
Nick Whitt <nick.whitt@protonmail.com>
2025-03-01 10:41:50 -0500

    blocks by bank

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -8,49 +8,55 @@
       Series 2021
     </h2>
 
-    <div class="my-4 flex items-center justify-between">
-      <h3 class="text-xl sm:text-2xl font-semibold tracking-tight text-pretty">
-        Boston
-      </h3>
-      <div class="flex gap-4 text-center">
-        <div>
-          <p class="text-nowrap">F-1999A</p>
-          <p>{{ formatNumber(57600000 - 1 + 1) }}</p>
-        </div>
-        <div>
-          <p class="text-nowrap">F-1999A*</p>
-          <p>{{ formatNumber(500000 - 1 + 1 + 4800000 - 3200001 + 1) }}</p>
+    <div v-for="bank in data" class="mb-8">
+      <div class="my-4 flex items-center justify-between">
+        <h3 class="text-xl sm:text-2xl font-semibold tracking-tight text-pretty">
+          {{ bank.bank.city }}
+        </h3>
+        <div class="flex gap-4 text-center">
+          <div>
+            <p class="text-nowrap">{{ bank.fNumber }}</p>
+            <p>{{ formatNumber(bank.regularNotes) }}</p>
+          </div>
+          <div>
+            <p class="text-nowrap">{{ bank.fNumber }}*</p>
+            <p>{{ formatNumber(bank.starNotes) }}</p>
+          </div>
         </div>
       </div>
-    </div>
 
-    <ol>
-      <li class="grid md:grid-cols-2 xl:grid-cols-3 gap-4">
-        <UCard :ui="{ body: 'p-0 sm:p-0', root: 'h-fit' }">
-          <template #header>
-            <h4 class="font-semibold">QAA Block</h4>
-          </template>
-          <UTable :data="data" :ui="{ thead: '[&>tr]:after:h-0' }">
-            <template #serials-cell="{ row }">
-              <ul class="font-mono">
-                <li>{{ formatSerialNumber(row.original.serials.start, { letter: 'A', city: 'Boston' }, 'Q') }}</li>
-                <li>{{ formatSerialNumber(row.original.serials.end, { letter: 'A', city: 'Boston' }, 'Q') }}</li>
-              </ul>
+      <ol class="grid md:grid-cols-2 xl:grid-cols-3 gap-4">
+        <li v-for="block in bank.blocks">
+          <UCard :ui="{ body: 'p-0 sm:p-0', root: 'h-fit' }">
+            <template #header>
+              <h4 class="font-semibold">
+                {{ bank.series + bank.bank.letter + block.block }} Block
+              </h4>
             </template>
-            <template #printed-cell="{ row }">
-              <ul class="text-right">
-                <li>{{ formatNumber(row.original.serials.end.serial - row.original.serials.start.serial + 1) }}</li>
-                <li>{{ row.original.printed }}</li>
-              </ul>
-            </template>
-          </UTable>
-        </UCard>
-      </li>
-    </ol>
+            <UTable :columns="columns" :data="block.dates" :ui="{ thead: '[&>tr]:after:h-0' }">
+              <template #serials-cell="{ row }">
+                <ul class="font-mono">
+                  <li>{{ formatSerialNumber(row.original.start, bank.bank, bank.series) }}</li>
+                  <li>{{ formatSerialNumber(row.original.end, bank.bank, bank.series) }}</li>
+                </ul>
+              </template>
+              <template #printed-cell="{ row }">
+                <ul class="text-right">
+                  <li>{{ formatNumber(row.original.end.serial - row.original.start.serial + 1) }}</li>
+                  <li>{{ row.original.printed }}</li>
+                </ul>
+              </template>
+            </UTable>
+          </UCard>
+        </li>
+      </ol>
+    </div>
   </UContainer>
 </template>
 
 <script setup lang="ts">
+import type { TableColumn } from '@nuxt/ui'
+
 type FederalReserveBank = {
   letter: string
   city: string
@@ -59,6 +65,18 @@ type FederalReserveBank = {
 type SerialBlock = {
   serial: number
   block: string
+}
+
+type PrintRun = {
+  date: string
+  printed: string
+  start: SerialBlock
+  end: SerialBlock
+}
+
+type Block = {
+  block: string
+  dates: PrintRun[]
 }
 
 function formatNumber(value: number): string {
@@ -80,8 +98,79 @@ function formatSerialNumber(serialBlock: SerialBlock, bank: FederalReserveBank, 
   return `${seriesLetter}${bank.letter} ${serialNumber.substring(0, 4)} ${serialNumber.substring(4)} ${serialBlock.block}`
 }
 
+const columns: TableColumn<PrintRun>[] = [
+  { accessorKey: 'date', header: 'Date' },
+  { id: 'serials', header: 'Serials' },
+  { accessorKey: 'printed', header: 'Printed', meta: { class: { th: 'text-right' } } },
+]
+
 const data = [
-  { date: 'Nov 2022', serials: { start: { serial: 1, block: 'A' }, end: { serial: 32000000, block: 'A' } }, printed: 'Fort Worth' },
-  { date: 'Dec 2022', serials: { start: { serial: 32000001, block: 'A' }, end: { serial: 57600000, block: 'A' } }, printed: 'Fort Worth' },
+  {
+    series: 'Q',
+    bank: { letter: 'A', city: 'Boston' },
+    fNumber: 'F-1999A',
+    regularNotes: 57600000,
+    starNotes: 500000 + 4800000 - 3200001 + 1,
+    blocks: [
+      {
+        block: 'A',
+        dates: [
+          {
+            date: 'Nov 2022',
+            printed: 'Fort Worth',
+            start: { serial: 1, block: 'A' },
+            end: { serial: 32000000, block: 'A' }
+          },
+          {
+            date: 'Dec 2022',
+            printed: 'Fort Worth',
+            start: { serial: 32000001, block: 'A' },
+            end: { serial: 57600000, block: 'A' },
+          },
+        ],
+      },
+      {
+        block: '*',
+        dates: [
+          {
+            date: 'Nov 2022',
+            printed: 'Fort Worth',
+            start: { serial: 1, block: '*' },
+            end: { serial: 500000, block: '*' }
+          },
+          {
+            date: 'Nov 2022',
+            printed: 'Fort Worth',
+            start: { serial: 3200000, block: '*' },
+            end: { serial: 4800000, block: '*' },
+          },
+        ],
+      },
+    ],
+  },
+  {
+    series: 'Q',
+    bank: { letter: 'L', city: 'San Francisco' },
+    fNumber: 'F-1999L',
+    blocks: [
+      {
+        block: 'A',
+        dates: [
+          {
+            date: 'Nov 2022',
+            printed: 'Fort Worth',
+            start: { serial: 1, block: 'A' },
+            end: { serial: 32000000, block: 'A' }
+          },
+          {
+            date: 'Dec 2022',
+            printed: 'Fort Worth',
+            start: { serial: 32000001, block: 'A' },
+            end: { serial: 89600000, block: 'A' },
+          },
+        ],
+      },
+    ],
+  },
 ]
 </script>
