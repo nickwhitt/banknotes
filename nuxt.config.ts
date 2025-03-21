14e78c3934f0a9a420a3d3edca2be4b8475db0f8
Nick Whitt <nick.whitt@protonmail.com>
2025-03-21 17:22:46 -0400

    dynamic routes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,4 +5,19 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
   future: { compatibilityVersion: 4 },
   compatibilityDate: '2024-11-27',
+  nitro: {
+    prerender: {
+      routes: [
+        '/series/s2021/notes/1',
+        '/series/g2021/notes/5',
+        '/series/g2021/notes/10',
+        '/series/g2021/notes/20',
+        '/series/g2021/notes/50',
+        '/series/g2021/notes/100',
+        '/series/s2017a/notes/1',
+        '/series/s2017a/notes/2',
+        '/series/g2017a/notes/20',
+      ],
+    },
+  },
 })
